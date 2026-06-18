8b7ec2fcb3d7bd0fdd071b68263c8fc9f8511067
Tanvan <tanvan.13022014@gmail.com>
2026-06-18 20:58:29 +0700

    feat(UserModel): add HasApiTokens

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -9,11 +9,12 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     public $incrementing = false;
     protected $keyType = 'string';
